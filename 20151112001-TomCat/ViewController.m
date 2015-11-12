5def8bd81f9e10e2baff6490ff1f689fb61febe3
raineryue <it_jinlei.yue@163.com>
2015-11-12 23:51:21 +0800

    不会说话的汤姆猫
    1.不会说话的汤姆猫，用来熟悉UIImageView控件，以及图片的动画播放。
    2.这里要注意内存的管理，图片内存的优化。

--- 20151112001-TomCat/ViewController.m
+++ 20151112001-TomCat/ViewController.m
@@ -10,18 +10,64 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *tomImageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+/**
+ *  按钮点击事件
+ */
+- (IBAction)didClickButtonAction:(UIButton *)sender {
+    // 1.判断是否正在执行动画，如果正在执行则不做任何操作
+    if (self.tomImageView.isAnimating) return;
+    
+    // 2.从plist文件中读取动画的图片
+    NSString *tomImagePath = [[NSBundle mainBundle] pathForResource:@"tom" ofType:@"plist"];
+    NSDictionary *tomDictionary = [NSDictionary dictionaryWithContentsOfFile:tomImagePath];
+    
+    // 3.获取当前按钮的标题，通过标题从字典中读取图片张数
+    NSString *buttonCurrentTitle = sender.currentTitle;
+    NSInteger count = [NSString stringWithFormat:@"%@", [tomDictionary objectForKey:buttonCurrentTitle]].integerValue;
+    
+    // 4.调用动画执行方法
+    [self tomAnimationsWithName:buttonCurrentTitle count:count];
+}
+
+/**
+ *  动画执行方法
+ */
+- (void)tomAnimationsWithName:(NSString *)name count:(NSInteger)count {
+    // 1.定一个一个数组用来装序列帧动画的图片组
+    NSMutableArray *imageArray = [NSMutableArray array];
+    
+    // 2.循环创建图片，并逐个添加到数组中
+    for (int i = 0; i < count; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"%@_%02d.jpg", name, i];
+        
+        NSString *imagePath = [[NSBundle mainBundle] pathForResource:imageName ofType:nil];
+        
+        UIImage *tomeImage = [UIImage imageWithContentsOfFile:imagePath];
+        
+        [imageArray addObject:tomeImage];
+    }
+    
+    // 3.设置序列帧动画属性
+    self.tomImageView.animationImages = imageArray;
+    self.tomImageView.animationRepeatCount = 1;
+    self.tomImageView.animationDuration = count * 0.075;
+    
+    // 4.开始动画
+    [self.tomImageView startAnimating];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
